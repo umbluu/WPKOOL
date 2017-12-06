2f1691e8759928822644cd9a5996bfcf5a42e448
andre <andre.pukk@khk.ee>
2017-12-06 23:52:50 +0200

    deleted plugin `2J Gallery` version 2.3.1

--- wp-content/plugins/blog-mechanics-theme-gallery/frontend/template/block/pagination/loadmore.php
+++
@@ -1,8 +0,0 @@
-<div class="load_more col-xs-12 text-center"
-     data-twoJG-page="<?php echo $page; ?>"
-     data-twoJG-per_page="<?php echo $perPage; ?>"
-     data-twoJG-pages="<?php echo $pages; ?>" >
-	<a href="#" class="btn btn-default">
-		<?php echo $buttonLoadMore; ?>
-	</a>
-</div>
