73555de5898acddbee477987b860bc26d83b5626
andre <andre.pukk@khk.ee>
2017-12-13 12:27:38 +0200

    deleted plugin `readme.txt` (after activation of `Theme My Login` version 6.4.9)

--- wp-content/plugins/blog-mechanics-theme-gallery/frontend/template/block/pagination/numbering.php
+++
@@ -1,5 +0,0 @@
-<div class="numbering twoJGalleryCSSwrap"
-     data-twoJG-page="<?php echo $page; ?>"
-     data-twoJG-per_page="<?php echo $perPage; ?>"
-     data-twoJG-pages="<?php echo $pages; ?>" >
-</div>
--- wp-content/plugins/blog-mechanics-theme-gallery/includes/extensions/fields/include/TwoJGalleryFieldsField/TwoJGalleryFieldsFieldTextImages.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-class TwoJGalleryFieldsFieldTextImages extends TwoJGalleryFieldsField{
-
-}
